9837eb5161d49d0d9dbb17b3f7b816d7a3513d98
HsingYun <hfmf520915@gmail.com>
2021-12-12 15:31:35 +0800

    recognize duration, offset, noise

--- guessWord.py
+++ guessWord.py
@@ -1,13 +1,20 @@
 import speech_recognition as sr
 
 r = sr.Recognizer()
-audiofile = sr.AudioFile('harvard.wav')
+clearfile = sr.AudioFile('harvard.wav')
+noisefile = sr.AudioFile('jackhammer.wav') # the stale smell of old beer lingers
 
 '''Audio file must be opened before record()'''
-with audiofile as source:
-    audio = r.record(source, duration=4) # 1-4sec file content
-    audio2 = r.record(source, duration=4) # 5-8sec file content
-    # audio = r.record(source) # whole file content
+# with clearfile as source:
+#     # audio = r.record(source) # whole file content
+#     audio1 = r.record(source, duration=4) # 1-4sec file content
+#     audio2 = r.record(source, duration=4) # 5-8sec file content
+#     audio3 = r.record(source, offset=3, duration=4) # after 3sec then record 4sec file content
+
+'''Deal noise'''
+with noisefile as source:
+    r.adjust_for_ambient_noise(source, duration=0.5)
+    audio = r.record(source)
 
 print(r.recognize_google(audio))
-print(r.recognize_google(audio2))
+# print(r.recognize_google(audio, show_all=True)) # show all possible recognition
